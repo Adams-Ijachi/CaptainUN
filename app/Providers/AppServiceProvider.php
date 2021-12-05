c357ef4a0e35420ab1b818d20ed533f77890fdaa
adam <adamsijachi9@gmail.com>
2021-12-05 15:27:35 +0100

    layout

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 use Laravel\Scout\Builder;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,6 +27,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
+        
         
     }
 }
