62b55d059d40bde5a0caad841ef6f65c786910ee
adam <adamsijachi9@gmail.com>
2021-12-05 15:05:55 +0100

    layout

--- resources/views/components/default-layout.blade.php
+++ resources/views/components/default-layout.blade.php
@@ -12,7 +12,7 @@
     <link rel="icon" type="image/png" sizes="16x16" href="{{ secure_asset('images/favicon.png') }}">
 
     <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous"> -->
-    <link href="{{ secure_secure_asset('css/style.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/style.css') }}" rel="stylesheet">
 
     
 </head>
@@ -45,11 +45,11 @@
     <!--**********************************
         Scripts
     ***********************************-->
-    <script src="{{ secure_secure_asset('plugins/common/common.min.js') }}"></script>
-    <script src="{{ secure_secure_asset('js/custom.min.js') }}"></script>
-    <script src="{{ secure_secure_asset('js/settings.js') }}"></script>
-    <script src="{{ secure_secure_asset('js/gleek.js') }}"></script>
-    <script src="{{ secure_secure_asset('js/styleSwitcher.js') }}"></script>
+    <script src="{{ secure_asset('plugins/common/common.min.js') }}"></script>
+    <script src="{{ secure_asset('js/custom.min.js') }}"></script>
+    <script src="{{ secure_asset('js/settings.js') }}"></script>
+    <script src="{{ secure_asset('js/gleek.js') }}"></script>
+    <script src="{{ secure_asset('js/styleSwitcher.js') }}"></script>
 </body>
 </html>
 
